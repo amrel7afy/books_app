e53af35be1091a2d646edc097019bafb5798e513
amrel7afy <ame.el7afy@gmail.com>
2024-02-08 03:43:37 +0200

    Creating the app structure and splash view.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'Features/splash/presentation/view/splash_view.dart';
+
+void main(){
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget{
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  return const GetMaterialApp(
+    home: SplashView(),
+  );
+  }
+}
