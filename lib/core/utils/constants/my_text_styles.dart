415a0144099c9867b5b2f6550d7268825a7fdb1c
amrel7afy <ame.el7afy@gmail.com>
2024-02-12 10:02:39 +0200

    Add font family and google fonts package

--- lib/core/utils/constants/my_text_styles.dart
+++ lib/core/utils/constants/my_text_styles.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'my_colors.dart';
 
-class MyTextStyles {
+abstract class MyTextStyles {
   static const headLine1 = TextStyle();
   static const headLine2 = TextStyle();
   static const headLine3 = TextStyle();
-  static const headLine4 = TextStyle();
+  static const headLine4 = TextStyle(
+    color: MyColors.kPrimaryColor,
+    fontSize: 18
+  );
   static const smallBody = TextStyle(
     color: MyColors.kPrimaryColor,
     fontWeight: FontWeight.w500
--- lib/core/utils/constants/theme.dart
+++ lib/core/utils/constants/theme.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'my_colors.dart';
 import 'my_text_styles.dart';
 
-ThemeData themeData = ThemeData.dark()
-    .copyWith(scaffoldBackgroundColor: MyColors.kBackGroundColor);
+ThemeData themeData = ThemeData.dark().copyWith(scaffoldBackgroundColor: MyColors.kBackGroundColor,
+
+textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme.copyWith(
+
+))
+);
