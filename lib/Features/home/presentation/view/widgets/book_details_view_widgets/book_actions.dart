0621096c91106d43669847ec73493b658c25b57f
amrel7afy <ame.el7afy@gmail.com>
2024-02-18 00:14:26 +0200

    Add book actions in details view.

---
+++ lib/Features/home/presentation/view/widgets/book_details_view_widgets/book_actions.dart
@@ -0,0 +1,38 @@
+import 'package:books_app/core/utils/constants/methods.dart';
+import 'package:books_app/core/utils/constants/my_colors.dart';
+import 'package:books_app/core/utils/constants/my_text_styles.dart';
+import 'package:books_app/core/utils/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class BookActions extends StatelessWidget {
+  const BookActions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: getWidth(context)*0.4,
+          child: CustomButton(
+            textStyle: MyTextStyles.boldTextStyle15,
+            backGroundColor: MyColors.kPrimaryColor,
+            onPressed: () {},
+            text: '19.99 .LE',
+            borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(13),topLeft: Radius.circular(13)),
+          ),
+        ),
+        SizedBox(
+          width: getWidth(context)*0.4,
+          child: CustomButton(
+            textStyle: MyTextStyles.boldTextStyle15.copyWith(color: MyColors.kPrimaryColor),
+            backGroundColor: const Color(0xffe87a64),
+            onPressed: () {},
+            text: 'Free Preview',
+            borderRadius: const BorderRadius.only(bottomRight: Radius.circular(13),topRight: Radius.circular(13)),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/Features/home/presentation/view/widgets/book_details_view_widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/view/widgets/book_details_view_widgets/book_details_view_body.dart
@@ -1,10 +1,13 @@
 import 'package:books_app/Features/home/presentation/view/widgets/book_details_view_widgets/book_details_app_bar.dart';
+import 'package:books_app/Features/home/presentation/view/widgets/book_details_view_widgets/book_actions.dart';
 import 'package:books_app/Features/home/presentation/view/widgets/home_view_widgets/best_seller_card.dart';
 import 'package:books_app/Features/home/presentation/view/widgets/home_view_widgets/book_image.dart';
 import 'package:books_app/core/utils/constants/constants.dart';
 import 'package:books_app/core/utils/constants/methods.dart';
+import 'package:books_app/core/utils/constants/my_colors.dart';
 import 'package:books_app/core/utils/constants/my_text_styles.dart';
 import 'package:books_app/core/utils/constants/vertical_and_horizontal_space.dart';
+import 'package:books_app/core/utils/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -18,9 +21,9 @@ class BookDetailsViewBody extends StatelessWidget {
       child: Column(
         children: [
           const CustomBookDetailsAppBar(),
-          const VerticalSpacer(15),
+          const VerticalSpacer(10),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: getWidth(context) * 0.2),
+            padding: EdgeInsets.symmetric(horizontal: getWidth(context) * 0.22),
             child: const BookImage(),
           ),
           const VerticalSpacer(20),
@@ -33,14 +36,21 @@ class BookDetailsViewBody extends StatelessWidget {
             child: Text(
               'Robert Sedgier',
               style: MyTextStyles.mediumTextStyle16.copyWith(
-                  fontStyle: FontStyle.italic,
+                fontStyle: FontStyle.italic,
               ),
-
             ),
           ),
-          const BookRating(mainAxisAlignment: MainAxisAlignment.center,)
+          const VerticalSpacer(20),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          const VerticalSpacer(20),
+          const BookActions(),
+
         ],
       ),
     );
   }
 }
+
+
