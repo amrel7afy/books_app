108e2ac99a9655766ec80cab36457ebb810f26a7
amrel7afy <ame.el7afy@gmail.com>
2024-03-17 02:20:19 +0200

    Add hive package for local data store, add hive object for book entity.

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,9 +1,19 @@
-class BookEntity{
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
+class BookEntity extends HiveObject{
+  @HiveField(0)
   final String img;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String author;
+  @HiveField(3)
   final num price;
+  @HiveField(4)
   final num rating;
+  @HiveField(5)
   final num ratingCount;
 
   BookEntity({required this.img, required this.title, required this.author, required this.price, required this.rating, required this.ratingCount});
